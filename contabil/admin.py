f8153d2d9f5be94e122455dd196730fce2152079
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2026-05-10 16:21:20 -0300

    Contruindo o banco de dados

--- contabil/admin.py
+++ contabil/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Conta, Operacao
 
-# Register your models here.
+@admin.register(Conta)
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ["nome", "criado_em"]
+    search_fields = ["nome"]
+
+@admin.register(Operacao)
+class OperacaoAdmin(admin.ModelAdmin):
+    list_display = ["descricao", "conta", "tipo", "valor", "dataoperacao"]
+    list_filter = ["tipo", "conta"]
+    search_fields = ["descricao", "conta__nome"]
+    date_hierarchy = "dataoperacao"
--- contabil/models.py
+++ contabil/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Conta(models.Model):
+    #Tabela de contas financeiras
+    nome = models.CharField(max_length=100, verbose_name="Nome da Conta")
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
+    criado_em = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    atualizado_em = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Conta"
+        verbose_name_plural = "Contas"
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
+class Operacao(models.Model):
+    TIPO_CHOICES = [
+        ("d", "Despesa"),
+        ("r", "Receita"),
+    ]
+    conta = models.ForeignKey(
+        Conta, 
+        on_delete=models.CASCADE, 
+        related_name="operacoes", 
+        verbose_name="Conta",
+    )
+    descricao = models.CharField(max_length=200, verbose_name="Descrição")
+    tipo = models.CharField(
+        max_length=1,
+        choices=TIPO_CHOICES,
+        default="d",
+        verbose_name="Tipo",
+    )
+    valor = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name="Valor"
+    )
+    dataoperacao = models.DateField(default=timezone.now, verbose_name="Data da Operação")
+    observacao = models.TextField(blank=True, null=True, verbose_name="Observações")
+    criado_em = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    atualizado_em = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+
+    class Meta:
+        verbose_name = "Operação"
+        verbose_name_plural = "Operações"
+        ordering = ["-dataoperacao", '-criado_em']
+
+    def __str__(self):
+        return f"{self.conta.nome - self.descricao}"
